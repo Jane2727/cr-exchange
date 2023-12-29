a4a47b9a1bd9bbe4195715942aaa01c6cee1cef2
Jane2727 <janej91@mail.ru>
2023-12-30 02:09:12 +0300

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/cr-exchange/",
   plugins: [react(), svgr()],
   define: {
     // Some libraries use the global object, even though it doesn't exist in the browser.
